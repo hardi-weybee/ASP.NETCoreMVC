92bbd07fbfcdb11e70565be971cad856229f372a
hardi-weybee <hardi@webtheuniverse.com>
2022-09-01 12:48:48 +0530

    added libman to use client side library

---
+++ DemoApplication/DemoApplication/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using DemoApplication.Models;
+using DemoApplication.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository = null;
+        public BookController()
+        {
+            bookRepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var data = bookRepository.GetAllBooks();
+
+            return View();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return bookRepository.GetBookByID(id);
+        }
+
+        public List<BookModel> SearchBook(string name, string authorName)
+        {
+            return bookRepository.SearchBook(name, authorName);
+        }
+    }
+}
---
+++ DemoApplication/DemoApplication/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using DemoApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookByID(int id)
+        {
+            return DataSource().Where(x => x.ID == id).FirstOrDefault(); 
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ ID =1, Title="Java", Author="Hardi"},
+                new BookModel(){ ID =2, Title="MVC", Author="Kakashi"},
+                new BookModel(){ ID =3, Title="C#", Author="Emma"},
+                new BookModel(){ ID =4, Title="PHP", Author="Charlie"},
+                new BookModel(){ ID =5, Title="DBMS", Author="Chandler"},
+            };
+        }
+    }
+}
